bc7dbe7feee77c6f1f02d55fb4ee6f6ecc03ecef
fraberret <f.berretta02@gmail.com>
2024-03-13 15:27:59 +0100

    create an array of todo and add on html page by a cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,20 @@
 console.log('it works');
 
+/*
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+*/
+
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: 'hello word'
+      toDoList: [
+        { text: "Fare la spesa", done: false },
+        { text: "Studiare per l'esame", done: false },
+        { text: "Fare una passeggiata", done: true }
+    ]
     }
   }
 }).mount('#app')
